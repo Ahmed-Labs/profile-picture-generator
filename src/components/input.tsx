9151e332c1592aee1f4e5e8fc5d0fd3ba59cf5bf
Ahmed-Labs <ahmedelhagjr@gmail.com>
2023-07-19 16:11:47 -0400

    set up genearte icon router, mutation, and a form to use the mutation

---
+++ src/components/input.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Input(props: React.ComponentPropsWithoutRef<"input">) {
+  return (
+    <input
+      {...props}
+      type="text"
+      className="rounded border border-gray-800 px-4 py-2 dark:text-gray-800"
+    ></input>
+  );
+}
+
+export default Input;
